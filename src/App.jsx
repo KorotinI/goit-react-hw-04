6999edd19b483cd764c0905e5ec4cab183e6ed38
KorotinI <142391798+KorotinI@users.noreply.github.com>
2024-04-01 17:52:43 +0200

    addapp

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,107 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import SearchBar from './components/SearchBar/SearchBar';
+import ImageGallery from './components/ImageGallery/ImageGallery';
+import Loader from './components/Loader/Loader';
+import LoadMoreBtn from './components/LoadMoreBtn/LoadMoreBtn';
+import ImageModal from './components/ImageModal/ImageModal';
+import ErrorMessage from './components/ErrorMessage/ErrorMessage';
+import { requestImages } from './services/unsplash-api';
+import { useState, useEffect, useRef } from 'react';
+import toast from 'react-hot-toast';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [query, setQuery] = useState('');
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [page, setPage] = useState(1);
+  const [modalIsOpen, setIsOpen] = useState(false);
+  const [modalUrl, setModalUrl] = useState('');
+  const [error, setError] = useState(false);
+  const [showBtn, setShowBtn] = useState(false);
+  const lastPhotoRef = useRef(null);
+
+  useEffect(() => {
+    async function fetchImages() {
+      if (!query.length) return;
+
+      try {
+        setIsLoading(true);
+        const fetchedImages = await requestImages(query, page);
+        const totalPages = fetchedImages.total_pages;
+
+        if (!fetchedImages.results.length) {
+          setIsLoading(false);
+          toast.error(
+            'Sorry, there are no images matching your search query. Please, try again!'
+          );
+
+          return;
+        }
+
+        setImages(images => [...images, ...fetchedImages.results]);
+        setShowBtn(totalPages > page);
+      } catch (error) {
+        setError(true);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    fetchImages();
+  }, [query, page]);
+
+  useEffect(() => {
+    if (!lastPhotoRef.current) return;
+
+    lastPhotoRef.current.scrollIntoView({
+      behavior: 'smooth',
+      block: 'end',
+    });
+  }, [images]);
+
+  const handleSearch = async query => {
+    setImages([]);
+    setError(false);
+    setShowBtn(false);
+    setQuery(query);
+    setPage(1);
+  };
+
+  const handleLoadMore = async () => {
+    setPage(p => p + 1);
+  };
+
+  const openModal = url => {
+    setModalUrl(url);
+    setIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsOpen(false);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <SearchBar onSearch={handleSearch} />
+      {images.length > 0 && !error ? (
+        <ImageGallery
+          images={images}
+          openModal={openModal}
+          ref={lastPhotoRef}
+        />
+      ) : (
+        error && <ErrorMessage />
+      )}
+      {images.length > 0 && !isLoading && showBtn && (
+        <LoadMoreBtn onClick={handleLoadMore} />
+      )}
+      {isLoading && <Loader />}
+      <ImageModal
+        modalUrl={modalUrl}
+        modalIsOpen={modalIsOpen}
+        closeModal={closeModal}
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
